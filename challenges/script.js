3f42d0cab0189e9a022d47bba54eb1f213a34139
Phil Peraza <phil.peraza@outlook.com>
2024-06-20 02:51:40 -0700

    vite practice project

---
+++ challenges/script.js
@@ -0,0 +1,10 @@
+function printEven(n) {
+  for(n = 0; n++){
+    if(n % 2 = 0){
+        console.log(n);
+    }
+  }
+  }
+  
+  printEven(10);
+  printEven(100);
---
+++ hello-vite/src/components/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <>
+    <Header />
+      <p>This is your first Vite project</p>
+    </>
+  );
+}
+
+export default App;
