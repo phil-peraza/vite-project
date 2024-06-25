ef07544b47da1046557fcdcc0beb3de4a2acd123
Phil Peraza <phil.peraza@outlook.com>
2024-06-25 02:22:10 -0700

    fix/ project source

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
+import headerLogo from '../assets/logo.png';
+
 function Header() {
     return (
         <div>
+            <img src={headerLogo} alt="TripleTen logo" />
             <h1>Hello Vite!</h1>
         </div>
     );
