4587028a7241df95d9f3ab9526f3039fdc959bc6
Phil Peraza <phil.peraza@outlook.com>
2024-06-25 03:32:26 -0700

    add/default image to page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Main from './Main';
+import Animal from './Animal';
 
 function App() {
   return (
     <>
     <Header />
     <Main />
+    <Animal />
     </>
   );
 }
